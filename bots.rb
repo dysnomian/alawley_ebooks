13cc7c9f02e2bb013cdd2de9030e976e7471f6c6
Liss McCabe <liss@eristiccode.com>
2016-02-10 14:05:23 -0800

    it works

--- bots.rb
+++ bots.rb
@@ -4,42 +4,55 @@ require 'twitter_ebooks'
 # You can define and instantiate as many bots as you like
 
 class MyBot < Ebooks::Bot
+  attr_accessor :original, :model, :model_path
+
   # Configuration here applies to all MyBots
   def configure
     # Consumer details come from registering an app at https://dev.twitter.com/
     # Once you have consumer details, use "ebooks auth" for new access tokens
-    self.consumer_key = '' # Your app consumer key
-    self.consumer_secret = '' # Your app consumer secret
+    self.consumer_key = 'I1BrefavDxNKw2LsAYwv9XsZB' # Your app consumer key
+    self.consumer_secret = 'Bnwl4m4M7WFJ3moxRNWaLKFgLXn4uRuxJxukbhCRXL4JooxtMg' # Your app consumer secret
 
     # Users to block instead of interacting with
-    self.blacklist = ['tnietzschequote']
+    self.blacklist = []
 
     # Range in seconds to randomize delay when bot.delay is called
     self.delay_range = 1..6
+
+    @userinfo = {}
   end
+
+  def top100; @top100 ||= model.keywords.take(100); end
+  def top20;  @top20  ||= model.keywords.take(20); end
 
   def on_startup
     scheduler.every '24h' do
       # Tweet something every 24 hours
       # See https://github.com/jmettraux/rufus-scheduler
-      # tweet("hi")
-      # pictweet("hi", "cuteselfie.jpg")
+      tweet(model.make_statement)
     end
   end
 
   def on_message(dm)
     # Reply to a DM
-    # reply(dm, "secret secrets")
+    delay do
+      reply(dm, model.make_response(dm.text))
+    end
   end
 
   def on_follow(user)
     # Follow a user back
-    # follow(user.screen_name)
+    follow(user.screen_name)
   end
 
   def on_mention(tweet)
     # Reply to a mention
-    # reply(tweet, "oh hullo")
+    # Become more inclined to pester a user when they talk to us
+    userinfo(tweet.user.screen_name).pesters_left += 1
+
+    delay do
+      reply(tweet, model.make_response(meta(tweet).mentionless, meta(tweet).limit))
+    end
   end
 
   def on_timeline(tweet)
@@ -54,12 +67,84 @@ class MyBot < Ebooks::Bot
 
   def on_retweet(tweet)
     # Follow user who just retweeted bot's tweet
-    # follow(tweet.user.screen_name)
+    return if tweet.retweeted_status?
+    return unless can_pester?(tweet.user.screen_name)
+
+    tokens = Ebooks::NLP.tokenize(tweet.text)
+
+    interesting = tokens.find { |t| top100.include?(t.downcase) }
+    very_interesting = tokens.find_all { |t| top20.include?(t.downcase) }.length > 2
+
+    delay do
+      if very_interesting
+        favorite(tweet) if rand < 0.5
+        retweet(tweet) if rand < 0.1
+        if rand < 0.01
+          userinfo(tweet.user.screen_name).pesters_left -= 1
+          reply(tweet, model.make_response(meta(tweet).mentionless, meta(tweet).limit))
+        end
+      elsif interesting
+        favorite(tweet) if rand < 0.05
+        if rand < 0.001
+          userinfo(tweet.user.screen_name).pesters_left -= 1
+          reply(tweet, model.make_response(meta(tweet).mentionless, meta(tweet).limit))
+        end
+      end
+    end
+  end
+
+  # Find information we've collected about a user
+  # @param username [String]
+  # @return [Ebooks::UserInfo]
+  def userinfo(username)
+    @userinfo[username] ||= UserInfo.new(username)
+  end
+
+  # Check if we're allowed to send unprompted tweets to a user
+  # @param username [String]
+  # @return [Boolean]
+  def can_pester?(username)
+    userinfo(username).pesters_left > 0
+  end
+
+  # Only follow our original user or people who are following our original user
+  # @param user [Twitter::User]
+  def can_follow?(username)
+    @original.nil? || username == @original || twitter.friendship?(username, @original)
+  end
+
+  def favorite(tweet)
+    if can_follow?(tweet.user.screen_name)
+      super(tweet)
+    else
+      log "Unfollowing @#{tweet.user.screen_name}"
+      twitter.unfollow(tweet.user.screen_name)
+    end
+  end
+
+  def on_follow(user)
+    if can_follow?(user.screen_name)
+      follow(user.screen_name)
+    else
+      log "Not following @#{user.screen_name}"
+    end
+  end
+
+  private
+  def load_model!
+    return if @model
+
+    @model_path ||= "model/#{original}.model"
+
+    log "Loading model #{model_path}"
+    @model = Ebooks::Model.load(model_path)
   end
 end
 
 # Make a MyBot and attach it to an account
 MyBot.new("alawley_ebooks") do |bot|
-  bot.access_token = "" # Token connecting the app to this account
-  bot.access_token_secret = "" # Secret connecting the app to this account
+  bot.access_token = "4889183160-xRIAL32nUA7vKSMbLXS0zexcHIKw48eNeRVYdfD"
+  bot.access_token_secret = "Q0JgY9stRLPyFQMRDeVgQBsfFtZDlI7buLMNO8QCIDQtQ"
+
+  bot.original = "alawley"
 end
